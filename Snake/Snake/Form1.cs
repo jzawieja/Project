0e841ee42d46c697d618c8fdfd12a16b82be58c6
jzawieja <joanna.kacprowska92@gmail.com>
2017-06-16 00:37:10 +0200

    Etap 4

--- Snake/Snake/Form1.cs
+++ Snake/Snake/Form1.cs
@@ -12,9 +12,31 @@ namespace Snake
 {
     public partial class Form1 : Form
     {
+        private List<Ractangle> Snake = new List<Ractangle>();
+        private Ractangle food = new Ractangle();
+
         public Form1()
         {
             InitializeComponent();
+
+            new Settings();
+
+            timer.Interval = 1000 / Settings.Speed;
+            timer.Tick += UpdateScreen();
+            timer.Start();
+
+            StartGame();
+        }
+        private void StartGame()
+        {
+            new Settings();
+
+            Snake.Clear();
+            Ractangle head = new Ractangle();
+            head.X = 20;
+            head.Y = 20;
+            Snake.Add(head);
+
         }
     }
 }
