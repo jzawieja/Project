bb55034a69d2c5d5efa014c9540f10737f471567
jzawieja <joanna.kacprowska92@gmail.com>
2017-06-16 10:37:47 +0200

    Etap 4

--- Snake/Snake/Form1.cs
+++ Snake/Snake/Form1.cs
@@ -37,6 +37,45 @@ namespace Snake
             head.Y = 20;
             Snake.Add(head);
 
+            realscore.Text = Settings.Score.ToString();
+            NewFood();
+
         }
+        private void NewFood()
+        {
+        int maxXpos = playingfield.Size.Width / Settings.Width;
+        int maxYpos = playingfield.Size.Height / Settings.Height;
+
+        Random random = new Random();
+        food = new Ractangle();
+        food.X = random.Next(0, maxXpos);
+        food.Y = random.Next(0, maxYpos);
+            }
+    private void UpdateScreen (object sender, EventArgs e)
+    {
+        if (Settings.GameOver == true)
+        {
+            if (Press.KeyPressed(Keys.Enter))
+            {
+                StartGame();
+            }
+        }
+        else
+            {
+                if (Press.KeyPressed(Keys.Right) && Settings.direction != Direction.left)
+                    Settings.direction = Direction.right;
+               else if (Press.KeyPressed(Keys.Left) && Settings.direction != Direction.right)
+                    Settings.direction = Direction.left;
+               else if (Press.KeyPressed(Keys.Up) && Settings.direction != Direction.down)
+                    Settings.direction = Direction.up;
+               else if (Press.KeyPressed(Keys.Down) && Settings.direction != Direction.up)
+                    Settings.direction = Direction.down;
+
+                MovePlayer();
+            }
+          
+
+    }
+
     }
 }
